229f7966a4a0782bdced1b43a1aed68c335c68d2
abdulmoiz509 <118448328+silentshaikh@users.noreply.github.com>
2023-08-28 16:16:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let memeBtn = document.querySelector(".btn");
+let memeTitle = document.getElementById("title");
+let memeAuthor = document.getElementById("author");
+let memeImg = document.getElementById("img-meme");
+
+let updateMeme = (url,title,author) => {
+    memeImg.src = url;
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+}
+const memeGenerator = async () => {
+    let myMeme = await fetch(`https://meme-api.com/gimme/wholesomememes`);
+    let myFetch = await myMeme.json();
+    // console.log(myFetch);
+    updateMeme(myFetch.url,myFetch.title,myFetch.author);
+}
+memeGenerator();
+memeBtn.addEventListener("click",memeGenerator);
